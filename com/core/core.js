64e2fb25466b641449026b377c133f22baadc7d0
Caner Akdaş <canerakdas@gmail.com>
2016-12-16 01:04:13 +0300

    Initial commit.

---
+++ com/core/core.js
@@ -0,0 +1,59 @@
+/**
+ * NEVER TRUST SCALAR
+ * @return {bool}
+ */
+scalar = function() {
+  try {
+    this.config = scalar.config;
+    this.selector = scalar.selector;
+
+    this.log = new Object();
+    this.log.error = new Array();
+    this.log.success = new Array();
+
+    document.createElement(this.selector.DOCUMENT.HEAD);
+    document.createElement(this.selector.DOCUMENT.BODY);
+
+    this._init();
+
+    this.log.success.push('Initialization complete : [../Scalar/com/core/core.js]');
+    return true;
+  } catch (e) {
+    return false;
+    this.log.error.push('Initialization rejected : [../Scalar/com/core/core.js]');
+  }
+}
+scalar.prototype = {
+  /**
+   * Initialization com/core/init/core.init.js
+   * @return {bool} 
+   */
+  _init: function() {
+    try {
+      this.htmlElement = document.createElement(this.config.INIT.ELEMENT);
+      this.htmlElement.type = this.config.INIT.TYPE;
+      this.htmlElement.src = this.config.INIT.SRC;
+      document.head.appendChild(this.htmlElement);
+      this.log.success.push('Initialization complete : [' + this.config.INIT.SRC + ']');
+      return true;
+    } catch (e) {
+      this.log.error.push('Initialization rejected : [' + this.config.INIT.SRC + ']');
+      return false;
+    }
+  }
+}
+
+scalar.config = {
+  INIT: {
+    ELEMENT: 'script',
+    TYPE: "text/javascript",
+    SRC: "../Scalar/com/core/init/core.init.js"
+  }
+}
+scalar.selector = {
+  DOCUMENT: {
+    BODY: 'body',
+    HEAD: 'head'
+  }
+}
+window.scalar = new scalar();
---
+++ com/core/init/core.init.js
@@ -0,0 +1,73 @@
+/**
+ * Initialize libraries
+ * @param  {object} object [scalar.lib]
+ * @return {bool}
+ */
+scalar.init = function(object) {
+  this.config = scalar.init.config;
+  try {
+    this.htmlElement = document.createElement(object.ELEMENT);
+    this.htmlElement.type = object.TYPE;
+    this.htmlElement.src = object.SRC;
+    document.head.appendChild(this.htmlElement);
+    scalar.log.success.push('Initialization complete : [' + object.SRC + ']');
+    return true;
+  } catch (e) {
+    this.log.error.push('Initialization rejected : [' + object.SRC + ']');
+    return false;
+  }
+}
+
+scalar.lib = {
+  UI: {
+    ELEMENT: 'script',
+    TYPE: "text/javascript",
+    SRC: "../Scalar/com/ui/ui.js"
+  },
+  COMPONENT: {
+    ELEMENT: 'script',
+    TYPE: "text/javascript",
+    SRC: "../Scalar/com/ui/component/ui.component.js"
+  },
+  DESKTOP: {
+    ELEMENT: 'script',
+    TYPE: "text/javascript",
+    SRC: "../Scalar/com/ui/desktop/ui.desktop.js"
+  },
+  WINDOW: {
+    ELEMENT: 'script',
+    TYPE: "text/javascript",
+    SRC: "../Scalar/com/ui/window/ui.window.js"
+  },
+  CALENDAR: {
+    ELEMENT: 'script',
+    TYPE: "text/javascript",
+    SRC: "../Scalar/com/ui/desktop/calendar/ui.desktop.calendar.js"
+  },
+  DOCK: {
+    ELEMENT: 'script',
+    TYPE: "text/javascript",
+    SRC: "../Scalar/com/ui/desktop/dock/ui.desktop.dock.js"
+  },
+  SEARCH: {
+    ELEMENT: 'script',
+    TYPE: "text/javascript",
+    SRC: "../Scalar/com/ui/desktop/search/ui.desktop.search.js"
+  },
+  WIDGET: {
+    ELEMENT: 'script',
+    TYPE: "text/javascript",
+    SRC: "../Scalar/com/ui/desktop/widget/ui.desktop.widget.js"
+  }
+}
+
+scalar.init.config = {};
+
+scalar.init(scalar.lib.UI);
+scalar.init(scalar.lib.COMPONENT);
+scalar.init(scalar.lib.DESKTOP);
+scalar.init(scalar.lib.DOCK);
+scalar.init(scalar.lib.CALENDAR);
+scalar.init(scalar.lib.SEARCH);
+scalar.init(scalar.lib.WIDGET);
+scalar.init(scalar.lib.WINDOW);
