3265238fc0fe06ce1f330fa634449adf30d95003
Hasan Ugur <hasan.ugur@segmentify.com>
2016-12-18 03:19:35 +0300

    Template moved electron

--- com/core/init/core.init.js
+++ com/core/init/core.init.js
@@ -6,11 +6,20 @@
 scalar.init = function(object) {
   this.config = scalar.init.config;
   try {
-    this.htmlElement = document.createElement(object.ELEMENT);
-    this.htmlElement.type = object.TYPE;
-    this.htmlElement.src = object.SRC;
-    document.head.appendChild(this.htmlElement);
-    scalar.log.success.push('Initialization complete : [' + object.SRC + ']');
+    if(object.TYPE=='text/css'){
+      this.htmlElement = document.createElement(object.ELEMENT);
+      this.htmlElement.type = object.TYPE;
+      this.htmlElement.href = object.SRC;
+      this.htmlElement.rel = 'stylesheet';
+      document.head.appendChild(this.htmlElement);
+      scalar.log.success.push('Initialization complete : [' + object.SRC + ']');
+    }else{
+      this.htmlElement = document.createElement(object.ELEMENT);
+      this.htmlElement.type = object.TYPE;
+      this.htmlElement.src = object.SRC;
+      document.head.appendChild(this.htmlElement);
+      scalar.log.success.push('Initialization complete : [' + object.SRC + ']');
+    }
     return true;
   } catch (e) {
     this.log.error.push('Initialization rejected : [' + object.SRC + ']');
@@ -58,6 +67,11 @@ scalar.lib = {
     ELEMENT: 'script',
     TYPE: "text/javascript",
     SRC: "../Scalar/com/ui/desktop/widget/ui.desktop.widget.js"
+  },
+  UI_FONT: {
+    ELEMENT: 'link',
+    TYPE: "text/css",
+    SRC: "../Scalar/com/ui/stylesheet/font.css"
   }
 }
 
--- com/ui/ui.js
+++ com/ui/ui.js
@@ -8,6 +8,8 @@ scalar.ui = function() {
   scalar.init(scalar.lib.SEARCH);
   scalar.init(scalar.lib.WIDGET);
   scalar.init(scalar.lib.WINDOW);
+  scalar.init(scalar.lib.UI_FONT);
+  
 }
 scalar.ui.config = {};
 scalar.ui.prototype = {
@@ -22,6 +24,8 @@ scalar.ui.prototype = {
         Object.keys(this.attribute).indexOf("background") >= 0 ? this.htmlElement.style.background = this.attribute.background : undefined;
         Object.keys(this.attribute).indexOf("text") >= 0 ? this.htmlElement.innerHTML = this.attribute.text : undefined;
         Object.keys(this.attribute).indexOf("href") >= 0 ? this.htmlElement.setAttribute("href", this.attribute.href) : undefined;
+        Object.keys(this.attribute).indexOf("placeholder") >= 0 ? this.htmlElement.setAttribute("placeholder", this.attribute.placeholder) : undefined;
+        Object.keys(this.attribute).indexOf("type") >= 0 ? this.htmlElement.setAttribute("type", this.attribute.type) : undefined;
       }
     }
     return this.htmlElement;
@@ -36,6 +40,37 @@ scalar.ui.prototype = {
   }
 };
 scalar.ui = new scalar.ui();
+
+
+scalar.ui.append('body',scalar.ui.createElement('div',{'id':'desktop'}));
+  scalar.ui.append('#desktop',scalar.ui.createElement('div',{'id':'wave'}));
+    scalar.ui.append('#wave',scalar.ui.createElement('input',{'id':'search','type':'text','placeholder':'Bilgisayarımda ara'}));
+    scalar.ui.append('#wave',scalar.ui.createElement('ul',{'id':'menu'}));
+      scalar.ui.append('#menu',scalar.ui.createElement('li',{'id':'calendar'}));
+        scalar.ui.append('#calendar',scalar.ui.createElement('div',{'id':'time'}));
+          scalar.ui.append('#time','18:45');
+        scalar.ui.append('#calendar',scalar.ui.createElement('div',{'id':'weather','class':'icon-snowy5'}));
+      scalar.ui.append('#menu',scalar.ui.createElement('li',{'id':'power'}));
+        scalar.ui.append('#power',scalar.ui.createElement('div',{'class':'icon-power_settings_new'}));
+      scalar.ui.append('#menu',scalar.ui.createElement('li',{'id':'user'}));
+        scalar.ui.append('#user',scalar.ui.createElement('div',{'class':'icon-person'}));
+      scalar.ui.append('#menu',scalar.ui.createElement('li',{'id':'config'}));
+        scalar.ui.append('#config',scalar.ui.createElement('div',{'class':'icon-settings'}));
+      scalar.ui.append('#menu',scalar.ui.createElement('li',{'id':'update'}));
+        scalar.ui.append('#update',scalar.ui.createElement('div',{'class':'icon-sync'}));
+
+  scalar.ui.append('#desktop',scalar.ui.createElement('div',{'id':'widget-container'}));
+    scalar.ui.append('#widget-container',scalar.ui.createElement('div',{'id':'widget-0','class':'content'}));
+
+    scalar.ui.append('#widget-container',scalar.ui.createElement('div',{'id':'widget-1','class':'content'}));
+
+    scalar.ui.append('#widget-container',scalar.ui.createElement('div',{'id':'widget-2','class':'content'}));
+
+  scalar.ui.append('#desktop',scalar.ui.createElement('div',{'id':'dock'}));
+    scalar.ui.append('#dock',scalar.ui.createElement('div',{'id':'dock-container'}));
+      scalar.ui.append('#dock-container',scalar.ui.createElement('span'));
+        scalar.ui.append('#dock-container span',scalar.ui.createElement('b'));
+
 
 scalar.exec('date "+%H:%M"', function(output) {
   console.log(output);
