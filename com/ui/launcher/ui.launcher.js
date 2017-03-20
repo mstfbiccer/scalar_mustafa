95cdb29047792847cf2df67e0fa2293c1bce6ded
Caner Akdaş <canerakdas@gmail.com>
2017-03-21 00:11:23 +0300

    Template : update

---
+++ com/ui/launcher/ui.launcher.js
@@ -0,0 +1,14 @@
+scalar.ui.launcher = function () {
+  this.config = scalar.ui.launcher.config;
+  scalar.ui.append('#launcher', scalar.ui.createElement('div', { 'id': 'a' }));
+};
+
+scalar.ui.launcher.config = {
+
+};
+scalar.ui.launcher.prototype = {
+};
+
+
+
+scalar.ui.launcher = new scalar.ui.launcher();
