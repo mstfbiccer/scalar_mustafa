e1f4ff2f37a05798c12dd8f6c743089a098a24ef
mstfbiccer <mstfbiccer@gmail.com>
2017-03-21 05:28:41 +0300

    Modified

--- com/ui/launcher/ui.launcher.js
+++ com/ui/launcher/ui.launcher.js
@@ -3,9 +3,12 @@ scalar.ui.launcher = function () {
   scalar.exec('cd com/bash && bash applicationList.sh', function (output) {
     output = '[' + output.slice(0, output.length - 2) + ']';
     window.asd = eval(output);
-    for (var i = 0; i < window.asd.length; i++) {
-      scalar.ui.append('#launcher', scalar.ui.createElement('div', { 'id': 'app-' + i,'text': window.asd[i].title }));
-      console.log(window.asd[i].title)
+    for (var i = 1; i < window.asd.length-2; i++) {
+     /* scalar.ui.append('#scroll-tab-'+i, scalar.ui.createElement('div', { 'class': 'mdl-gird s'+i}));
+      scalar.ui.append('.mdl-gird.s'+i, scalar.ui.createElement('div', { 'class': 'mdl-cell mdl-cell--1-col s'+i}));
+      document.querySelector('.mdl-cell.mdl-cell--1-col.s'+i).innerHTML=window.asd[i].title;*/
+      /*document.querySelector("a[href='#scroll-tab-"+i+"']").innerHTML=window.asd[i].title.slice(0,15)+"...";
+      console.log(window.asd[i].title)*/
     }
   });
 };
--- main.js
+++ main.js
@@ -19,11 +19,11 @@ const express = require('./myserver');
      //fullscreen:true
    })
    // and load the index.html of the app.
-win.loadURL("http://localhost:2016/index_mustafa.html");
+win.loadURL("http://localhost:2016");
 
 
    // Open the DevTools.
-   //win.webContents.openDevTools()
+   win.webContents.openDevTools()
 
    // Emitted when the window is closed.
    win.on('closed', () => {
